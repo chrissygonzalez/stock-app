0241049973f2abed2be8cbdcb7da37055edd5017
chrissygonzalez <chrissygonzalez@gmail.com>
2019-06-29 22:28:47 -0500

    get basic font colors based on change since open

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -9,12 +9,19 @@ class StocksController < ApplicationController
         @stock_quantities = Hash.new
         client = IEX::Api::Client.new(publishable_token: ENV['IEX_API_PUBLISHABLE_TOKEN'])
 
+        # TODO: some of this prob needs to move into a separate function in the model
         @stocks.each do |stock|
             quantity = 0
+            compared_to_open = ""
             @user.transactions.where(stock_id: stock.id).each do |txn|
                 quantity = quantity + txn.quantity
             end
-            @stock_quantities[stock.symbol] = [quantity, client.price(stock.symbol)]
+            if client.price(stock.symbol) > client.ohlc(stock[:symbol]).open.price
+                compared_to_open = "up-since-open"
+            elsif client.price(stock.symbol) < client.ohlc(stock[:symbol]).open.price
+                compared_to_open = "down-since-open"
+            end
+            @stock_quantities[stock.symbol] = [quantity, client.price(stock.symbol), compared_to_open]
         end
 
         @transaction = Transaction.new(user_id: current_user.id)
