d7ce251715d730d7e9e8866b9b5d9139dc65ef4b
chrissygonzalez <chrissygonzalez@gmail.com>
2020-01-26 16:18:21 -0500

    adjust table row html element and styling

--- app/assets/javascripts/components/stockRow.jsx
+++ app/assets/javascripts/components/stockRow.jsx
@@ -1,12 +1,13 @@
 class StockRow extends React.Component {
     render() {
+        let dollarStyle = `align-right ${this.props.style}`;
       return (
-        <tr>
-            <td class={this.props.style}>{this.props.symbol}</td> 
-            <td class={this.props.style}>{this.props.change}</td>
+        <React.Fragment>
+            <td className={this.props.style}>{this.props.symbol}</td> 
+            <td className={this.props.style}>{this.props.change}</td>
             <td>{this.props.quantity}</td>
-            <td class={this.props.style}>${this.props.value}</td>
-        </tr>
+            <td className={dollarStyle}>${this.props.value}</td>
+        </React.Fragment>
         )
     }
   }
